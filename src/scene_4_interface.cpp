ff7974dc335ecea6538bfb84819e481e229ebd09
Guilherme Cattani <gcattani1@gmail.com>
2021-02-08 20:27:08 -0300

    Add plot var custom widget

--- src/scene_4_interface.cpp
+++ src/scene_4_interface.cpp
@@ -13,8 +13,8 @@ void InterfaceScene4::Show()
   ImGui::SliderFloat3("C", (float *)&g_Scene4->c, -3.0f, 3.0f);
   ImGui::SliderFloat3("D", (float *)&g_Scene4->d, -3.0f, 3.0f);
 
-  ImGui::PlotLines("T", g_Scene4->t_arr, IM_ARRAYSIZE(g_Scene4->t_arr));
-  ImGui::PlotLines("X", g_Scene4->bezier_arr_x, IM_ARRAYSIZE(g_Scene4->bezier_arr_x));
-  ImGui::PlotLines("Y", g_Scene4->bezier_arr_y, IM_ARRAYSIZE(g_Scene4->bezier_arr_y));
-  ImGui::PlotLines("Z", g_Scene4->bezier_arr_z, IM_ARRAYSIZE(g_Scene4->bezier_arr_z));
+  ImGui::PlotVar("Current T", g_Scene4->t);
+  ImGui::PlotVar("Current X", g_Scene4->x);
+  ImGui::PlotVar("Current Y", g_Scene4->y);
+  ImGui::PlotVar("Current Z", g_Scene4->z);
 }
