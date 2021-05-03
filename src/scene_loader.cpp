374fec46790b8d3c109b379df79123d3c34bcbf6
Guilherme Cattani <gcattani1@gmail.com>
2021-05-02 22:08:52 -0300

    Unload shaders after linking

--- src/scene_loader.cpp
+++ src/scene_loader.cpp
@@ -3,30 +3,43 @@
 #include "scene_loader.h"
 #endif
 
+
 void CreateProgramForScene(int scene)
 {
+  // We delete the program because it unloads shaders completely
+  if( g_ProgramId != 0 ) {
+    glDeleteProgram(g_ProgramId); g_ProgramId = 0;
+  }
+
   switch (scene)
   {
   case 2:
-    // Criamos um programa de GPU utilizando os shaders carregados acima
-    g_ProgramId = CreateGpuProgram(
-        LoadShader_Vertex(Scene2::shader_vertex_filepath),
-        LoadShader_Fragment(Scene2::shader_fragment_filepath));
+    g_sceneVertexShader = LoadShader_Vertex(Scene2::shader_vertex_filepath);
+    g_sceneFragmentShader = LoadShader_Fragment(Scene2::shader_fragment_filepath);
     break;
   case 3:
-    // Criamos um programa de GPU utilizando os shaders carregados acima
-    g_ProgramId = CreateGpuProgram(
-        LoadShader_Vertex(Scene3::shader_vertex_filepath),
-        LoadShader_Fragment(Scene3::shader_fragment_filepath));
+    g_sceneVertexShader = LoadShader_Vertex(Scene3::shader_vertex_filepath);
+    g_sceneFragmentShader = LoadShader_Fragment(Scene3::shader_fragment_filepath);
     break;
   case 4:
-    // Criamos um programa de GPU utilizando os shaders carregados acima
-    g_ProgramId = CreateGpuProgram(
-        LoadShader_Vertex(Scene4::shader_vertex_filepath),
-        LoadShader_Fragment(Scene4::shader_fragment_filepath));
+    g_sceneVertexShader = LoadShader_Vertex(Scene4::shader_vertex_filepath);
+    g_sceneFragmentShader = LoadShader_Fragment(Scene4::shader_fragment_filepath);
     break;
 
   default:
     printf("No scene given to create, idling.\n");
   }
+  // Criamos um programa de GPU utilizando os shaders carregados acima
+  g_ProgramId = CreateGpuProgram(g_sceneVertexShader, g_sceneFragmentShader);
+  if( g_sceneVertexShader != 0 ) {
+    glDetachShader(g_ProgramId, g_sceneVertexShader);
+    glDeleteShader(g_sceneVertexShader);
+    g_sceneVertexShader = 0;
+    }
+  if( g_sceneFragmentShader != 0 ) {
+    glDetachShader(g_ProgramId, g_sceneFragmentShader);
+    glDeleteShader(g_sceneFragmentShader);
+    g_sceneFragmentShader = 0;
+  }
+
 }
