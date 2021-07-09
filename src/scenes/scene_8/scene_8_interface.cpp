8d075167d49a3f07126f29687d5df6bac710e508
Guilherme Cattani <gcattani1@gmail.com>
2021-07-09 16:52:16 -0300

    Add field of view slider to scene 8

--- src/scenes/scene_8/scene_8_interface.cpp
+++ src/scenes/scene_8/scene_8_interface.cpp
@@ -10,15 +10,24 @@
 
 void InterfaceScene8::ShowControls()
 {
-  ImGui::Checkbox("Free Camera Toggle", &g_Scene8->camera->isFreeCamera);
+  if(g_Scene8->camera->isFreeCamera) {
+    ImGui::Checkbox("Free Camera Toggle", &g_Scene8->camera->isFreeCamera);
+  } else {
+    ImGui::Checkbox("Look at Camera Toggle", &g_Scene8->camera->isFreeCamera);
+  }
+
+    if(!g_Scene8->camera->isFreeCamera)
+  {
+    ImGui::SliderFloat3("Look At", (float *)&g_Scene8->camera->lookAt, -3.0f, 3.0f);
+    ImGui::SliderFloat3("Camera Position", (float *)&g_Scene8->camera->position, -10.0f, 10.0f);
+  }
+  ImGui::SliderFloat("Camera Near Plane", &g_Scene8->camera->nearPlane, -10.0f, 10.0f);
+  ImGui::SliderFloat("Camera Far Plane", &g_Scene8->camera->farPlane, -10.0f, 10.0f);
+  ImGui::SliderFloat("Field Of View", &g_Scene8->camera->fieldOfView, 0.0f, 3.14f);
+
   ImGui::Text("Camera Pos: %f %f %f", g_Scene8->camera->position.x,
                                       g_Scene8->camera->position.y,
                                       g_Scene8->camera->position.z);
-  ImGui::SliderFloat("Camera Near Plane", &g_Scene8->camera->nearPlane, -100.0f, 100.0f);
-  ImGui::SliderFloat("Camera Far Plane", &g_Scene8->camera->farPlane, -100.0f, 100.0f);
-
-  ImGui::SliderFloat3("Look At", (float *)&g_Scene8->camera->lookAt, -3.0f, 3.0f);
-  ImGui::SliderFloat3("Camera Position", (float *)&g_Scene8->camera->position, -10.0f, 10.0f);
 
   ImGui::Separator();
   ImGui::Text("Second Camera Pos: %f %f %f", g_Scene8->second_camera->position.x,
